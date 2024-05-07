07198b5b5b703000e1930959f4071b5d69f05bd1
nurazima <nurazima270@gmail.com>
2024-05-07 19:46:04 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1 :class="{ 'red-text': isRed }">Dynamic Interaction with Vue</h1>
+    <p :style="{ fontSize: fontSize + 'px' }">Change font size:</p>
+    <input type="range" min="10" max="50" v-model="fontSize">
+    <br>
+    <input type="text" v-model="inputText" @input="updateText">
+    <p>You typed: {{ inputText }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fontSize: 16,
+      inputText: '',
+      isRed: false
+    };
+  },
+  methods: {
+    updateText() {
+      this.isRed = this.inputText.length > 10;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.red-text {
+  color: red;
+}
+</style>
